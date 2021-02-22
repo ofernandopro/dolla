042dd38b65b11f8006b5f3b6b9e4b881eac73bed
Fernando Moreira <fernandoepm.pro@gmail.com>
2021-02-22 20:35:13 -0300

    Initializing project and creating Navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import { 
+  MobileIcon, 
+  Nav, 
+  NavbarContainer, 
+  NavItem, 
+  NavLinks, 
+  NavLogo, 
+  NavMenu 
+} from './NavbarElements';
+
+function Navbar() {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">dolla</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu> 
+            <NavItem>
+              <NavLinks to='about'>About</NavLinks>
+            </NavItem>
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
